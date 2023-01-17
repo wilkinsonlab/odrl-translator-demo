a7782fee9fb4c11cac8965690cf4d463eec37aeb
Oussama Benhamed <wisleur@gmail.com>
2023-01-17 15:53:04 +0100

    feat: add reverso-api typings

--- server/src/types/reverso_api.d.ts
+++ server/src/types/reverso_api.d.ts
@@ -1,1 +1,47 @@
-declare module "reverso-api";
+declare module "reverso-api" {
+  global {
+    export interface ReversoResponse {
+      ok: boolean;
+      text: string;
+      source: string;
+      target: string;
+      translations: Array<string>;
+      examples: {
+        id: number;
+        source: string;
+        target: string;
+      }[];
+    }
+
+    export interface ReversoErrorResponse {
+      ok: boolean;
+      message: string;
+    }
+  }
+
+  type CallbackFunction = (
+    error: ReversoErrorResponse,
+    response: ReversoResponse
+  ) => void;
+
+  export default class Reverso {
+    constructor({
+      insecureHTTPParser = false,
+    }?: {
+      insecureHTTPParser: boolean;
+    });
+    getContext(
+      text: string,
+      source: string,
+      target: string,
+      callback?: CallbackFunction
+    ): ReversoResponse;
+
+    getTranslation(
+      text: string,
+      source: string,
+      target: string,
+      callback?: CallbackFunction
+    ): ReversoResponse;
+  }
+}
