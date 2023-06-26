c17a08b9b30ea245e836cdff9dc798779a766649
Mark Wilkinson <markw@illuminae.com>
2023-06-26 06:30:56 +0200

    simple policy

--- negotiator/implementation/lib/negotiator.rb
+++ negotiator/implementation/lib/negotiator.rb
@@ -3,8 +3,9 @@ require 'linkeddata'
 offer = ARGV[0] || "https://wilkinsonlab.github.io/odrl-translator-demo/policies/offer-biohackathon.jsonld"
 
 graph = RDF::Graph.load(offer)
-query = SPARQL.parse("select DISTINCT ?s ?type where {?s a ?type}")
+
+query = SPARQL.parse("select DISTINCT ?s where {?s a <http://www.w3.org/ns/odrl/2/Offer>}")
 r = query.execute(graph)
 r.each do |r|
-    puts r[:type]
+    puts r[:s]
 end
